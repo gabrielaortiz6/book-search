c19e6e73907377491ce5c2e1af06471dbd1fe7ec
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-07-15 21:11:20 -0400

    create user working

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -31,7 +31,6 @@ export const ADD_USER = gql`
         _id
         username
         email
-        bookCount
         savedBooks {
           bookId
           authors
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
